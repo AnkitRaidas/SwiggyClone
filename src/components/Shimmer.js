14c435e1ae0acd23398309d8df9c3b91d97a31e9
Ankit Raidas <ankitraidas001@gmail.com>
2026-06-23 00:44:48 +0530

    Shimmer

---
+++ src/components/Shimmer.js
@@ -0,0 +1,226 @@
+export default function Shimmer(){
+
+
+    return(
+    //     <div>
+    //       <div className="w-[280px] mb-2 animate-pulse">
+    //           <div className="w-full h-45 rounded-2xl bg-gray-300"></div>
+    //           <div className="w-[95%] mx-auto mt-3">
+    //             <div className="w-[80%] h-6 rounded bg-gray-300"></div>
+    //             <div className="w-[60%] h-6 rounded bg-gray-300 mt-2"></div>
+    //             <div className="w-[90] h-6 rounded bg-gray-300 mt-2"></div>
+    //           </div>
+    //     </div>
+  
+        
+    // </div>
+
+<div className="flex flex-wrap w-[80%] mx-auto mt-20 gap-5">
+
+    <div className="w-[280px] mb-2 animate-pulse">
+  <div className="w-full h-45 rounded-2xl bg-gray-300"></div>
+
+  <div className="mt-3">
+    <div className="w-[80%] h-5 bg-gray-300 rounded"></div>
+
+    <div className="flex items-center gap-2 mt-3">
+      <div className="w-5 h-5 rounded-full bg-gray-300"></div>
+      <div className="w-12 h-4 bg-gray-300 rounded"></div>
+      <div className="w-16 h-4 bg-gray-300 rounded"></div>
+    </div>
+
+    <div className="w-[90%] h-4 bg-gray-300 rounded mt-3"></div>
+    <div className="w-[60%] h-4 bg-gray-300 rounded mt-2"></div>
+  </div>
+</div>
+
+  <div className="w-[280px] mb-2 animate-pulse">
+  <div className="w-full h-45 rounded-2xl bg-gray-300"></div>
+
+  <div className="mt-3">
+    <div className="w-[80%] h-5 bg-gray-300 rounded"></div>
+
+    <div className="flex items-center gap-2 mt-3">
+      <div className="w-5 h-5 rounded-full bg-gray-300"></div>
+      <div className="w-12 h-4 bg-gray-300 rounded"></div>
+      <div className="w-16 h-4 bg-gray-300 rounded"></div>
+    </div>
+
+    <div className="w-[90%] h-4 bg-gray-300 rounded mt-3"></div>
+    <div className="w-[60%] h-4 bg-gray-300 rounded mt-2"></div>
+  </div>
+</div>
+
+  <div className="w-[280px] mb-2 animate-pulse">
+  <div className="w-full h-45 rounded-2xl bg-gray-300"></div>
+
+  <div className="mt-3">
+    <div className="w-[80%] h-5 bg-gray-300 rounded"></div>
+
+    <div className="flex items-center gap-2 mt-3">
+      <div className="w-5 h-5 rounded-full bg-gray-300"></div>
+      <div className="w-12 h-4 bg-gray-300 rounded"></div>
+      <div className="w-16 h-4 bg-gray-300 rounded"></div>
+    </div>
+
+    <div className="w-[90%] h-4 bg-gray-300 rounded mt-3"></div>
+    <div className="w-[60%] h-4 bg-gray-300 rounded mt-2"></div>
+  </div>
+</div>
+
+  <div className="w-[280px] mb-2 animate-pulse">
+  <div className="w-full h-45 rounded-2xl bg-gray-300"></div>
+
+  <div className="mt-3">
+    <div className="w-[80%] h-5 bg-gray-300 rounded"></div>
+
+    <div className="flex items-center gap-2 mt-3">
+      <div className="w-5 h-5 rounded-full bg-gray-300"></div>
+      <div className="w-12 h-4 bg-gray-300 rounded"></div>
+      <div className="w-16 h-4 bg-gray-300 rounded"></div>
+    </div>
+
+    <div className="w-[90%] h-4 bg-gray-300 rounded mt-3"></div>
+    <div className="w-[60%] h-4 bg-gray-300 rounded mt-2"></div>
+  </div>
+</div>
+
+  <div className="w-[280px] mb-2 animate-pulse">
+  <div className="w-full h-45 rounded-2xl bg-gray-300"></div>
+
+  <div className="mt-3">
+    <div className="w-[80%] h-5 bg-gray-300 rounded"></div>
+
+    <div className="flex items-center gap-2 mt-3">
+      <div className="w-5 h-5 rounded-full bg-gray-300"></div>
+      <div className="w-12 h-4 bg-gray-300 rounded"></div>
+      <div className="w-16 h-4 bg-gray-300 rounded"></div>
+    </div>
+
+    <div className="w-[90%] h-4 bg-gray-300 rounded mt-3"></div>
+    <div className="w-[60%] h-4 bg-gray-300 rounded mt-2"></div>
+  </div>
+</div>
+
+  <div className="w-[280px] mb-2 animate-pulse">
+  <div className="w-full h-45 rounded-2xl bg-gray-300"></div>
+
+  <div className="mt-3">
+    <div className="w-[80%] h-5 bg-gray-300 rounded"></div>
+
+    <div className="flex items-center gap-2 mt-3">
+      <div className="w-5 h-5 rounded-full bg-gray-300"></div>
+      <div className="w-12 h-4 bg-gray-300 rounded"></div>
+      <div className="w-16 h-4 bg-gray-300 rounded"></div>
+    </div>
+
+    <div className="w-[90%] h-4 bg-gray-300 rounded mt-3"></div>
+    <div className="w-[60%] h-4 bg-gray-300 rounded mt-2"></div>
+  </div>
+</div>
+
+  <div className="w-[280px] mb-2 animate-pulse">
+  <div className="w-full h-45 rounded-2xl bg-gray-300"></div>
+
+  <div className="mt-3">
+    <div className="w-[80%] h-5 bg-gray-300 rounded"></div>
+
+    <div className="flex items-center gap-2 mt-3">
+      <div className="w-5 h-5 rounded-full bg-gray-300"></div>
+      <div className="w-12 h-4 bg-gray-300 rounded"></div>
+      <div className="w-16 h-4 bg-gray-300 rounded"></div>
+    </div>
+
+    <div className="w-[90%] h-4 bg-gray-300 rounded mt-3"></div>
+    <div className="w-[60%] h-4 bg-gray-300 rounded mt-2"></div>
+  </div>
+</div>
+
+  <div className="w-[280px] mb-2 animate-pulse">
+  <div className="w-full h-45 rounded-2xl bg-gray-300"></div>
+
+  <div className="mt-3">
+    <div className="w-[80%] h-5 bg-gray-300 rounded"></div>
+
+    <div className="flex items-center gap-2 mt-3">
+      <div className="w-5 h-5 rounded-full bg-gray-300"></div>
+      <div className="w-12 h-4 bg-gray-300 rounded"></div>
+      <div className="w-16 h-4 bg-gray-300 rounded"></div>
+    </div>
+
+    <div className="w-[90%] h-4 bg-gray-300 rounded mt-3"></div>
+    <div className="w-[60%] h-4 bg-gray-300 rounded mt-2"></div>
+  </div>
+</div>
+
+  <div className="w-[280px] mb-2 animate-pulse">
+  <div className="w-full h-45 rounded-2xl bg-gray-300"></div>
+
+  <div className="mt-3">
+    <div className="w-[80%] h-5 bg-gray-300 rounded"></div>
+
+    <div className="flex items-center gap-2 mt-3">
+      <div className="w-5 h-5 rounded-full bg-gray-300"></div>
+      <div className="w-12 h-4 bg-gray-300 rounded"></div>
+      <div className="w-16 h-4 bg-gray-300 rounded"></div>
+    </div>
+
+    <div className="w-[90%] h-4 bg-gray-300 rounded mt-3"></div>
+    <div className="w-[60%] h-4 bg-gray-300 rounded mt-2"></div>
+  </div>
+</div>
+
+  <div className="w-[280px] mb-2 animate-pulse">
+  <div className="w-full h-45 rounded-2xl bg-gray-300"></div>
+
+  <div className="mt-3">
+    <div className="w-[80%] h-5 bg-gray-300 rounded"></div>
+
+    <div className="flex items-center gap-2 mt-3">
+      <div className="w-5 h-5 rounded-full bg-gray-300"></div>
+      <div className="w-12 h-4 bg-gray-300 rounded"></div>
+      <div className="w-16 h-4 bg-gray-300 rounded"></div>
+    </div>
+
+    <div className="w-[90%] h-4 bg-gray-300 rounded mt-3"></div>
+    <div className="w-[60%] h-4 bg-gray-300 rounded mt-2"></div>
+  </div>
+</div>
+
+  <div className="w-[280px] mb-2 animate-pulse">
+  <div className="w-full h-45 rounded-2xl bg-gray-300"></div>
+
+  <div className="mt-3">
+    <div className="w-[80%] h-5 bg-gray-300 rounded"></div>
+
+    <div className="flex items-center gap-2 mt-3">
+      <div className="w-5 h-5 rounded-full bg-gray-300"></div>
+      <div className="w-12 h-4 bg-gray-300 rounded"></div>
+      <div className="w-16 h-4 bg-gray-300 rounded"></div>
+    </div>
+
+    <div className="w-[90%] h-4 bg-gray-300 rounded mt-3"></div>
+    <div className="w-[60%] h-4 bg-gray-300 rounded mt-2"></div>
+  </div>
+</div>
+
+  <div className="w-[280px] mb-2 animate-pulse">
+  <div className="w-full h-45 rounded-2xl bg-gray-300"></div>
+
+  <div className="mt-3">
+    <div className="w-[80%] h-5 bg-gray-300 rounded"></div>
+
+    <div className="flex items-center gap-2 mt-3">
+      <div className="w-5 h-5 rounded-full bg-gray-300"></div>
+      <div className="w-12 h-4 bg-gray-300 rounded"></div>
+      <div className="w-16 h-4 bg-gray-300 rounded"></div>
+    </div>
+
+    <div className="w-[90%] h-4 bg-gray-300 rounded mt-3"></div>
+    <div className="w-[60%] h-4 bg-gray-300 rounded mt-2"></div>
+  </div>
+</div>
+
+</div>
+    )
+}
